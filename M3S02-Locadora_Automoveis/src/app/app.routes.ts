c2e779f147ec397c0bab2d868c007d7e9d60cc87
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-08-30 19:36:23 +0200

    [M3S02] Ex 02 - Adaptação de Roteamento

--- M3S02-Locadora_Automoveis/src/app/app.routes.ts
+++ M3S02-Locadora_Automoveis/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AutomoveisComponent } from './pages/automoveis/automoveis.component';
+import { AutomoveisDetailsComponent } from './pages/automoveis/automoveis-details/automoveis-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'automoveis',
+    pathMatch: 'full',
+  },
+  {
+    path: 'automoveis',
+    children: [
+      {
+        path: '',
+        component: AutomoveisComponent,
+      },
+      {
+        path: ':id',
+        component: AutomoveisDetailsComponent,
+      },
+    ],
+  },
+];
