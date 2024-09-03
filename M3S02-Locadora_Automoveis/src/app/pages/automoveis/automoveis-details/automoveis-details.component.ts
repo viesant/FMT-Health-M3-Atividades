ac7bf20b7864b7a4e886d57aa72f6457cdb35467
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-09-03 22:14:54 +0200

    [M3S02] Ex 05 - Desenvolvimento da Tela de Detalhamento

--- M3S02-Locadora_Automoveis/src/app/pages/automoveis/automoveis-details/automoveis-details.component.ts
+++ M3S02-Locadora_Automoveis/src/app/pages/automoveis/automoveis-details/automoveis-details.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AutomoveisService } from '../../../services/automoveis.service';
+import { ActivatedRoute } from '@angular/router';
+import { YearAutoPipe } from '../../../pipes/year-auto.pipe';
 
 @Component({
   selector: 'app-automoveis-details',
   standalone: true,
-  imports: [],
+  imports: [YearAutoPipe],
   templateUrl: './automoveis-details.component.html',
-  styleUrl: './automoveis-details.component.css'
+  styleUrl: './automoveis-details.component.css',
 })
 export class AutomoveisDetailsComponent {
+  automovel: any = {};
+  automoveisService = inject(AutomoveisService);
+  activatedRoute = inject(ActivatedRoute);
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe({
+      next: (params: any) => {
+        this.automoveisService.detail(params.id).subscribe({
+          next: (data) => {
+            this.automovel = data;
+          },
+          error: (error) => {
+            console.log('Erro do detail', error);
+          },
+        });
+      },
+      error: (error) => {
+        console.log('Erro do params', error);
+      },
+    });
+  }
 }
