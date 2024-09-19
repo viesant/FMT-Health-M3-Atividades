d20b8e677e6af4f692c439c9374f4c5e2cea6504
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-09-19 23:21:41 +0200

    [M3S04] Start project

---
+++ M3S04-SuggestionBox/src/main/java/com/viesant/M3S04_SuggestionBox/M3S04SuggestionBoxApplication.java
@@ -0,0 +1,13 @@
+package com.viesant.M3S04_SuggestionBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3S04SuggestionBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3S04SuggestionBoxApplication.class, args);
+	}
+
+}
