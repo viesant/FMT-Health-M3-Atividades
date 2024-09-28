995175f70059d6c746ceee71f4430c0119d10d50
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-09-28 12:00:37 +0200

    [M3S05] Ex 7 - Teste de Integração com ConsutaController

--- M3S05-Health_testesUnitarios/codigo_base/src/test/java/br/com/exemplo/aula/controllers/ConsultaControllerTest.java
+++ M3S05-Health_testesUnitarios/codigo_base/src/test/java/br/com/exemplo/aula/controllers/ConsultaControllerTest.java
@@ -7,25 +7,21 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import br.com.exemplo.aula.controllers.dto.ConsultaResponseDTO;
 import br.com.exemplo.aula.entities.Consulta;
 import br.com.exemplo.aula.entities.Nutricionista;
 import br.com.exemplo.aula.entities.Paciente;
 import br.com.exemplo.aula.repositories.ConsultaRepository;
 import java.time.LocalDate;
 import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
